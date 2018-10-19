9dcb4da4931a1f6d2639573b5143cdd336375771
Henry Mi <henry.mi@cargosmart.com>
2018-10-19 17:43:51 +0800

    Simple Linked List Practice

---
+++ apps/JavaBase/src/datastructure/list/SimpleLinkedListTest.java
@@ -0,0 +1,17 @@
+package datastructure.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by MIHE on 10/19/2018.
+ */
+public class SimpleLinkedListTest {
+
+  public static void main(String args[]){
+    List<String> linkedList = new LinkedList<>();
+//    linkedList.
+
+  }
+
+}
