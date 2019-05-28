b74d4106d92495798acdd71b6464e27a63b5d724
Henry Mi <henry.mi@cargosmart.com>
2019-05-28 10:59:58 +0800

    write some cases of ServletDemo

--- apps/SerlvetDemo/ServletDemo1/src/web/servlet/ServletDemo3.java
+++ apps/SerlvetDemo/ServletDemo1/src/web/servlet/ServletDemo3.java
@@ -21,6 +21,12 @@ public class ServletDemo3 implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("Servlet annotation!");
+        String name = servletRequest.getParameter("name");
+        servletResponse.setContentType("text/html; charset=UTF-8");
+        ServletOutputStream outputStream = servletResponse.getOutputStream();
+        outputStream.write((name + " connect to servlet annotation!").getBytes());
+        outputStream.flush();
+        outputStream.close();
     }
 
     @Override
