cfc6ed4a1b4f6be74ae741f48177219c89ca8561
Henry Mi <henry.mi@cargosmart.com>
2019-06-11 11:48:32 +0800

    spring boot configuration test

--- apps/SpringBootDemo/src/main/java/com/mian/SpringBootDemo/controller/PropertiesController.java
+++ apps/SpringBootDemo/src/main/java/com/mian/SpringBootDemo/controller/PropertiesController.java
@@ -1,18 +1,30 @@
 package com.mian.SpringBootDemo.controller;
 
+import com.mian.SpringBootDemo.configuration.CustomConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/properties")
+@EnableConfigurationProperties(CustomConfiguration.class)
 public class PropertiesController {
 
     @Value("${server.port}")
     private String port;
+    @Autowired
+    private CustomConfiguration customConfiguration;
 
     @RequestMapping("/port")
     public String getPort() {
         return port;
+    }
+
+    @RequestMapping("/master")
+    public String getMaster() {
+        return "[" + customConfiguration.getName() + ":" + customConfiguration.getAge() + "]";
     }
 
 }
