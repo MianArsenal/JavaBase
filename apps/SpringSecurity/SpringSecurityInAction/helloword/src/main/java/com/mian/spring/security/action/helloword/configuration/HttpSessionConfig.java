c0b6a3bdee65ebb878a0a6e8893b848ecb9dbe56
Henry Mi <henry.mi@cargosmart.com>
2020-07-29 17:13:39 +0800

    spring security -- refactor

--- apps/SpringSecurity/SpringSecurityInAction/helloword/src/main/java/com/mian/spring/security/action/helloword/configuration/HttpSessionConfig.java
+++ apps/SpringSecurity/SpringSecurityInAction/helloword/src/main/java/com/mian/spring/security/action/helloword/configuration/HttpSessionConfig.java
@@ -1,18 +1,18 @@
 package com.mian.spring.security.action.helloword.configuration;
 
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 //import org.springframework.data.redis.connection.RedisConnectionFactory;
 //import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 //import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.security.web.session.HttpSessionEventPublisher;
 //import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 //@EnableRedisHttpSession
 public class HttpSessionConfig {
 
-    /*
-    * 默认localhost:6379
-    * */
+//    /*
+//    * 默认localhost:6379
+//    * */
 //    @Bean
 //    public RedisConnectionFactory redisConnectionFactory() {
 //        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
@@ -23,9 +23,9 @@ public class HttpSessionConfig {
 //    }
 
 
-//    @Bean
-//    public HttpSessionEventPublisher httpSessionEventPublisher () {
-//        return new HttpSessionEventPublisher();
-//    }
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher () {
+        return new HttpSessionEventPublisher();
+    }
 
 }
--- apps/SpringSecurity/SpringSecurityInAction/helloword/src/main/java/com/mian/spring/security/action/helloword/configuration/WebSecurityConfigSessionConcurrent.java
+++ apps/SpringSecurity/SpringSecurityInAction/helloword/src/main/java/com/mian/spring/security/action/helloword/configuration/WebSecurityConfigSessionConcurrent.java
@@ -1,8 +1,6 @@
 package com.mian.spring.security.action.helloword.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
@@ -13,31 +11,35 @@ import org.springframework.session.security.SpringSessionBackedSessionRegistry;
 //@Configuration
 //@RequiredArgsConstructor
 public class WebSecurityConfigSessionConcurrent extends WebSecurityConfigurerAdapter {
-//
-//    private final FindByIndexNameSessionRepository<? extends Session> sessionRepository;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/admin/api/**").hasRole("ADMIN")
-//                .antMatchers("/user/api/**").hasRole("USER")
-//                .antMatchers("/app/api/**", "/captcha.jpg").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .csrf().disable()
-//                .formLogin()
-//                .and()
-//                .sessionManagement()
-//                .maximumSessions(1)
-//                .sessionRegistry(sessionRegistry());
-//    }
-//
-//    /*
-//     * 用于在集群环境下控制session并发的session注册表实现类
-//     */
-//    @Bean
-//    public SessionRegistry sessionRegistry() {
-//        return new SpringSessionBackedSessionRegistry<>(sessionRepository);
-//    }
+
+    private final FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+
+    public WebSecurityConfigSessionConcurrent(FindByIndexNameSessionRepository<? extends Session> sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/api/**").hasRole("ADMIN")
+                .antMatchers("/user/api/**").hasRole("USER")
+                .antMatchers("/app/api/**", "/captcha.jpg").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .and()
+                .sessionManagement()
+                .maximumSessions(1)
+                .sessionRegistry(sessionRegistry());
+    }
+
+    /*
+     * 用于在集群环境下控制session并发的session注册表实现类
+     */
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SpringSessionBackedSessionRegistry<>(sessionRepository);
+    }
 
 }
